5085171c24f0451c94b20abba7710daf3b3ee61a
Pseudo-Paul <plle@ucdavis.edu>
2025-02-17 14:56:12 -0800

    yes

--- proj2/testsrc/CXMLReaderTest.cpp
+++ proj2/testsrc/CXMLReaderTest.cpp
@@ -7,11 +7,16 @@ int main() {
     std::shared_ptr<CStringDataSource> source = std::make_shared<CStringDataSource>(xmlData);
     CXMLReader reader(source);
 
+    std::cout << "Starting XML Parsing..." << std::endl;
+
     SXMLEntity entity;
     while (reader.ReadEntity(entity)) {
-        std::cout << "Type: " << (entity.DType == SXMLEntity::EType::StartElement ? "Start" : "End") 
-                  << ", Name: " << entity.DNameData << std::endl;
+        std::cout << "Entity Read: " 
+                  << ((entity.DType == SXMLEntity::EType::StartElement) ? "Start" : 
+                     (entity.DType == SXMLEntity::EType::EndElement) ? "End" : "Complete") 
+                  << " | Name: " << entity.DNameData << std::endl;
     }
 
+    std::cout << "XML Parsing Completed." << std::endl;
     return 0;
 }
