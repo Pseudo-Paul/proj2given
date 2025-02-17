add80be959c0086722cdd45b816967fb2b8c33a4
Pseudo-Paul <plle@ucdavis.edu>
2025-02-17 15:01:13 -0800

    yea

--- proj2/testsrc/CXMLReaderTest.cpp
+++ proj2/testsrc/CXMLReaderTest.cpp
@@ -10,13 +10,16 @@ int main() {
     std::cout << "Starting XML Parsing..." << std::endl;
 
     SXMLEntity entity;
+    int count = 0; // Count how many times ReadEntity() is called
     while (reader.ReadEntity(entity)) {
+        count++;
         std::cout << "Entity Read: " 
                   << ((entity.DType == SXMLEntity::EType::StartElement) ? "Start" : 
                      (entity.DType == SXMLEntity::EType::EndElement) ? "End" : "Complete") 
                   << " | Name: " << entity.DNameData << std::endl;
     }
 
+    std::cout << "ReadEntity was called " << count << " times." << std::endl;
     std::cout << "XML Parsing Completed." << std::endl;
     return 0;
 }
