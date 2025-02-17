20bb0f6d2bbf46df4b548e6c7bfd6910b53c5a52
Pseudo-Paul <plle@ucdavis.edu>
2025-02-17 14:58:14 -0800

    yes please

--- proj2/src/CXMLReader.cpp
+++ proj2/src/CXMLReader.cpp
@@ -9,10 +9,12 @@ CXMLReader::CXMLReader(std::shared_ptr<CDataSource> src)
     XML_SetUserData(parser, this);
     XML_SetElementHandler(parser, StartElementHandler, EndElementHandler);
     XML_SetCharacterDataHandler(parser, CharacterDataHandler);
+    std::cout << "CXMLReader initialized.\n"; // Debug message
 }
 
 CXMLReader::~CXMLReader() {
     XML_ParserFree(parser);
+    std::cout << "CXMLReader destroyed.\n"; // Debug message
 }
 
 bool CXMLReader::End() const {
@@ -20,11 +22,16 @@ bool CXMLReader::End() const {
 }
 
 bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipCDATA) {
+    std::cout << "ReadEntity() called...\n"; // Debug message
+
     char buffer[1024];
     std::vector<char> readBuffer;
 
     if (source->Read(readBuffer, sizeof(buffer))) {
         std::memcpy(buffer, readBuffer.data(), readBuffer.size());
+        
+        std::cout << "Parsing buffer: " << std::string(buffer, readBuffer.size()) << std::endl; // Debug output
+
         if (XML_Parse(parser, buffer, readBuffer.size(), source->End()) == XML_STATUS_ERROR) {
             std::cerr << "XML Parsing Error: " << XML_ErrorString(XML_GetErrorCode(parser)) << std::endl;
             return false;
@@ -32,16 +39,20 @@ bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipCDATA) {
         entity = currentEntity;
         return true;
     }
+
+    std::cout << "End of XML file reached.\n"; // Debug message
     endOfFile = true;
     return false;
 }
 
 void CXMLReader::StartElementHandler(void *userData, const char *name, const char **atts) {
     CXMLReader *reader = static_cast<CXMLReader*>(userData);
+    std::cout << "Start Element: " << name << std::endl;
+
     reader->currentEntity.DType = SXMLEntity::EType::StartElement;
     reader->currentEntity.DNameData = name;
-
     reader->currentEntity.DAttributes.clear();
+
     for (int i = 0; atts[i]; i += 2) {
         reader->currentEntity.DAttributes.push_back({atts[i], atts[i + 1]});
     }
@@ -49,12 +60,16 @@ void CXMLReader::StartElementHandler(void *userData, const char *name, const cha
 
 void CXMLReader::EndElementHandler(void *userData, const char *name) {
     CXMLReader *reader = static_cast<CXMLReader*>(userData);
+    std::cout << "End Element: " << name << std::endl;
+
     reader->currentEntity.DType = SXMLEntity::EType::EndElement;
     reader->currentEntity.DNameData = name;
 }
 
 void CXMLReader::CharacterDataHandler(void *userData, const char *data, int len) {
     CXMLReader *reader = static_cast<CXMLReader*>(userData);
+    std::cout << "Character Data: " << std::string(data, len) << std::endl;
+
     reader->currentEntity.DType = SXMLEntity::EType::CompleteElement;
     reader->currentEntity.DNameData.assign(data, len);
 }
