1f25ad69c4460d24312babad7dc7be7ce75121ae
Pseudo-Paul <plle@ucdavis.edu>
2025-02-17 14:47:47 -0800

    please work

---
+++ proj2/testsrc/CDSVWriterTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../include/CDSVWriter.h"
+#include "../include/StringDataSink.h"
+
+int main() {
+    std::shared_ptr<CStringDataSink> sink = std::make_shared<CStringDataSink>();
+    CDSVWriter writer(sink, ',', false);
+
+    std::vector<std::string> row1 = {"Alice", "25", "New York"};
+    std::vector<std::string> row2 = {"Bob", "30", "Los Angeles"};
+
+    writer.WriteRow(row1);
+    writer.WriteRow(row2);
+
+    std::cout << "Written Data:\n" << sink->String() << std::endl;
+
+    return 0;
+}
